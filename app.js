61b770a475e152e61529b56315090de411145b3a
DanielJ0131 <daniel.jonsson0131@gmail.com>
2025-04-03 14:04:39 +0200

    Project Structure, Express Server

---
+++ app.js
@@ -0,0 +1,23 @@
+import { app } from './src/express.js'
+import databaseService from './src/service/DatabaseService.js'
+
+// Connect to the database
+await databaseService.connect()
+
+// Start the express server
+const port = process.env.PORT || 3000
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+/**
+ * Shut down all services.
+ */
+async function shutdown () {
+  server.close()
+  await databaseService.closeConnection()
+}
+
+process.on('SIGINT', shutdown)
+process.on('SIGTERM', shutdown)
