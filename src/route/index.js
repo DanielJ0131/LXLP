505fb58a36d486c8646b1302d2c9d61876ca3a55
DanielJ0131 <daniel.jonsson0131@gmail.com>
2025-04-03 16:25:56 +0200

    Comment database connections, fix errorHandler

---
+++ src/route/index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+// import { router as apikeyRoute } from './apikeyRoute.js'
+// import { router as jwtRoute } from './jwtRoute.js'
+// import { router as usersRoute } from './usersRoute.js'
+// This will be the main route connecting all the other routes
+// once we have jwt, api and user database routes.
+
+export const router = express.Router()
