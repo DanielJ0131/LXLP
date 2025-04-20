cdc6f3adf86df588b72bfe8498ba11f19a6588fe
ryadhazin0002 <159729344+ryadhazin0002@users.noreply.github.com>
2025-04-20 18:57:57 +0200

    Refactor DatabaseService to use mongoose for database operations and created new schemas for users, posts, comments

--- src/service/DatabaseService.js
+++ src/service/DatabaseService.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import mongoose, {Schema} from 'mongoose'
 import { config } from '../config/database.js'
 
 /**
@@ -7,52 +7,63 @@ import { config } from '../config/database.js'
  * @class
  */
 class DatabaseService {
-  #client = null
   #db = null
+  userSchema = null
+  postScema = null
+  commentSchema = null
+  users = null
+  posts = null
+  comments = null
 
-  /**
-   * Connect to the MongoDB database using the configuration provided.
-   *
-   * @async
-   * @returns {Promise<void>}
-   */
-  async connect () {
+  async connect() {
+    console.log('Trying to Connect to Database...');
     try {
-      this.#client = new MongoClient(config.url)
-      await this.#client.connect()
-      this.#db = this.#client.db() // Use the default database from the connection string
+      this.#db = await mongoose.connect(config.url);
+      console.log('Connection Successful!');
+
+      // Fill the user schema
+      this.userSchema = new Schema({
+        name: String,
+        email: String,
+        password: String,
+        Image: String,
+        role: String,
+      });
+      // Fill the post schema
+      this.postScema = new Schema({
+        userId:{$olid: String},
+        content: String,
+        status: String,
+        likes: Number,
+        dislikes: Number
+      });
+      // Fill the comment schema
+      this.commentSchema = new Schema({
+        postId: {$olid: String},
+        userId: {$olid: String},
+        content: String,
+        likes: Number,
+        dislikes: Number
+      });
+
+      // Use the collection with created schemas
+      this.users = mongoose.model('users', this.userSchema);
+      this.posts = mongoose.model('posts', this.postScema);
+      this.comments = mongoose.model('comments', this.commentSchema);
     } catch (error) {
-      console.error('Error connecting to MongoDB:', error.message)
-      throw error
+      console.log('Error with connection: ' + error);
     }
   }
 
-  /**
-   * Get a reference to a collection.
-   *
-   * @param {string} collectionName - The name of the collection.
-   * @returns {object} The MongoDB collection.
-   */
-  getCollection (collectionName) {
-    if (!this.#db) {
-      throw new Error('Database connection is not established.')
-    }
-    return this.#db.collection(collectionName)
-  }
-
-  /**
-   * Close the MongoDB connection.
-   *
-   * @async
-   */
-  async closeConnection () {
-    try {
-      if (this.#client) {
-        await this.#client.close()
+  async closeConnection() {
+    if (this.#db) {
+      try {
+        await mongoose.disconnect();
+        console.log('Database connection terminated.');
+        
+      } catch (error) {
+        console.log('Error while disconnecting: ' + error);
       }
-    } catch (err) {
-      console.error('Error closing the MongoDB connection:', err.message)
-      throw err
     }
   }
 }
