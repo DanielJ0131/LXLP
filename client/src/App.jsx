3a6d0ef4a1c3fe11fe090123c0773e40e1d1f25c
DanielJ0131 <daniel.jonsson0131@gmail.com>
2025-04-30 15:53:18 +0200

    Add package.json and client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <header>
+        <h1>Linux Learning Platform</h1>
+      </header>
+      <main>MAIN MAIN</main>
+      <footer>
+        <p>© LXLP. All Rights Reserved</p>
+        <a href="mailto:linuxlearningplatform@gmail.com">linuxlearningplatform@gmail.com</a>
+      </footer>
+    </div>
   )
 }
 
