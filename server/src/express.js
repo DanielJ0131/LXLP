73cd1758e4dfabc78b338117e6fc9dd2ee51853f
local <macutsergej@gmail.com>
2025-05-08 01:28:00 +0200

    Change redirect to client... uninstall node-pty

--- server/src/express.js
+++ server/src/express.js
@@ -4,13 +4,13 @@ import logger from 'morgan'
 import {router} from './route/index.js'
 import {errorHandler} from './middleware/errorHandler.js'
 import cors from 'cors' // Import cors for cross-origin resource sharing
-import WSS_Server from "./webSocketServer.js";
+// import WSS_Server from "./webSocketServer.js";
 
 
 export const app = express()
 
 // Start Websocket server for terminal
-WSS_Server.startServer(app)
+// WSS_Server.startServer(app)
 
 // Use the morgan logger
 if (process.env.NODE_ENV !== 'test') {
@@ -27,16 +27,26 @@ app.use(cors())
 app.disable('x-powered-by')
 
 // Use the public folder for static resources
-app.use(express.static('public'))
+// app.use(express.static('public'))
+
+// Use the public folder for static resources
+app.use(express.static('../client/dist'))
 
 // Middleware to parse JSON data as part of the body
 app.use(express.json())
 
 // Redirect from the server's root to the client's /home
-app.get('/', (req, res) => {
-    res.redirect('http://localhost:5173/home');
-});
+// app.get('/', (req, res) => {
+//     res.redirect('http://localhost:5173/home');
+// });
 
+app.use((req, res, next) => {
+    res.sendFile('index.html', { root: '../client/dist' }, (err) => {
+        if (err) {
+            next(err)
+        }
+    })
+})
 
 // Mount the routes
 app.use('/', router)
