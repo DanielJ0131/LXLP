d8e3e11bb0fe3d7f409d75b05287985f3484bfbb
ryadhazin0002 <159729344+ryadhazin0002@users.noreply.github.com>
2025-05-12 20:50:04 +0200

    Add role-based access control middleware and integrate it into user update route

--- server/src/route/usersRoute.js
+++ server/src/route/usersRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import UsersController from '../controller/UserController.js'
+import {checkRole} from '../middleware/roleMiddelwere.js'
 
 export const router = express.Router()
 
@@ -8,8 +9,11 @@ router.get('/:id', (req, res, next) => UsersController.getUserById(req, res, nex
 router.get('/', (req, res, next) => UsersController.getAllUsers(req, res, next))
 router.get('/name/:name', (req, res, next) => UsersController.getUserByName(req, res, next))
 router.get('/email/:email', (req, res, next) => UsersController.getUserByEmail(req, res, next))
-router.get('/username/:username', (req, res, next) => UsersController.getUserByUsername(req, res, next))
+router.get('/username/:username', UsersController.getUserByUsername)
 router.post('/login', (req, res, next) => UsersController.loginUser(req, res, next))
 router.post('/', (req, res, next) => UsersController.addUser(req, res, next))
-router.put('/:id', (req, res, next) => UsersController.updateUser(req, res, next))
+
+
+
+router.put('/:id',checkRole(["user", "admin"]), UsersController.updateUser)
 router.delete('/:id', (req, res, next) => UsersController.deleteUser(req, res, next))
