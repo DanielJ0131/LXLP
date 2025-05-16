b31445d813244316455355336f871ee52105ebaf
TonyMNG <tony.nguyen0004@stud.hkr.se>
2025-05-16 12:16:25 +0200

    added profile page feature

---
+++ client/src/components/profile.jsx
@@ -0,0 +1,16 @@
+/* import { useEffect, useState } from 'react';
+import { fetchWithAuth } from './http.js';  */
+
+export default function Profile({ user }) {
+  if (!user) return <div>Loading...</div>;
+
+  return (
+    <div className="profile-container">
+      <h1>{user.firstname}'s Profile</h1>
+      <p>Status: {user.role === 'admin' ? 'Admin' : 'User'}</p>
+      <p>Email: {user.email}</p>
+      <p>Full Name: {user.firstname} {user.lastname}</p>
+      
+    </div>
+  );
+}
