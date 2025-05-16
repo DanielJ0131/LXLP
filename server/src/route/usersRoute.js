11b781d1707dcdb39e1929e0934cc8a6f7be25e3
ryadhazin0002 <159729344+ryadhazin0002@users.noreply.github.com>
2025-05-16 20:58:07 +0200

    Add role-based access control to comments and users routes

--- server/src/route/usersRoute.js
+++ server/src/route/usersRoute.js
@@ -7,7 +7,7 @@ export const router = express.Router()
 
 router.use(jwtMiddleware.jwtTokenIsValid)
 
-router.get('/:id', UsersController.getUserById)
+router.get('/:id', checkRole(["admin", "users"]), UsersController.getUserById)
 router.get('/', checkRole(["admin"]),UsersController.getAllUsers)
 router.get('/name/:name', checkRole(["admin","user"]), UsersController.getUserByName)
 router.get('/email/:email',checkRole(["admin", "user"]), UsersController.getUserByEmail)
