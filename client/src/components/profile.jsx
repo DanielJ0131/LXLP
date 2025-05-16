de5cecebcf7215bd7b3ec67772617e8bf5b710d9
TonyMNG <tony.nguyen0004@stud.hkr.se>
2025-05-16 15:39:18 +0200

    profile layout update

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -2,11 +2,11 @@
 import { fetchWithAuth } from './http.js';  */
 
 export default function Profile({ user }) {
-  if (!user) return <div>Loading...</div>;
-
+  if (!user) return <div>Login to view profile.</div>;
+    
   return (
     <div className="profile-container">
-      <h1>{user.firstname}'s Profile</h1>
+      <h1>{user.username}'s Profile</h1>
       <p>Status: {user.role === 'admin' ? 'Admin' : 'User'}</p>
       <p>Email: {user.email}</p>
       <p>Full Name: {user.firstname} {user.lastname}</p>
