08e305661da0dd3867c5f692bbb341acb1965365
padthe <n.patrick040@gmail.com>
2025-05-17 13:29:02 +0200

    Added toggle password in password reset

--- client/src/components/resetPassword.jsx
+++ client/src/components/resetPassword.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { FaEye, FaEyeSlash } from "react-icons/fa"
+
+
 
 export default function ResetPassword() {
   const [newPassword, setNewPassword] = useState('')
   const [message, setMessage] = useState('')
   const [searchParams] = useSearchParams()
   const token = searchParams.get('token')
+  const [showPassword, setShowPassword] = useState(false)
+
+
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -23,18 +29,28 @@ export default function ResetPassword() {
     }
   }
 
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Change password</h2>
-      <input
-        type="password"
-        placeholder="New password"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-        required
-      />
+      <div className='login-input'>
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="New password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          required
+        />
+        <button
+          type="button"
+          className="toggle-password"
+          onClick={() => setShowPassword(prev => !prev)}
+        >
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </button>
+      </div>
       <button type="submit">Change password</button>
-      {message && <p>{message}. Redirecting to login.</p>}
+      {message && <p>{message} Redirecting to login.</p>}
     </form>
   )
 }
